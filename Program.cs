fb7be09e45d11b5eb834bf318cad7b852bda9c7c
Xylina Delgado <chain.userxd@gmail.com>
2026-01-29 15:32:53 -0700

    Feet to inches program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine(FeetToInches(30));
+    Console.WriteLine(FeetToInches(60));
+   int FeetToInches(int Feet)
+    {
+      int inches = Feet * 12;
+      return inches;
+   }
+  }
+}
